8f3d850978b18c007fb6983c2e5ac35c2c6ce2b8
EngEmadHany <emadhany2007@gmail.com>
2023-08-03 00:01:13 +0400

    Initial Commit

---
+++ SwiftUIActionSheet/SwiftUIActionSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIActionSheetApp.swift
+//  SwiftUIActionSheet
+//
+//  Created by Emad Isaac on 20/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIActionSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
